d7653219e303560afad76ea749412517ac858922
akshat-61 <akshat74269@gmail.com>
2024-12-22 22:04:41 +0530

    Update

--- project.py
+++ project.py
@@ -4,6 +4,7 @@ import csv
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
 # # This is used to create an animated, static and interactive visualistaion in python
 import seaborn as sns
 # It is built on top of matplotlib and is used to design creative attractive and informative statistical graphics
@@ -51,6 +52,16 @@ plt.ylabel('Unemployment Rate')
 plt.xticks(rotation=45)
 plt.show()
 
+
+# Thirdly we'll check the Unemployment rate by Gender
+
+plt.figure(figsize=(10, 6))
+sns.barplot(x='gender', y='Unemployment Rate', data=data)
+plt.title('Unemployment Rate by gender')
+plt.xlabel('Gender')
+plt.ylabel('Unemployment Rate')
+plt.show()
+
 # Now we will do Correlation Analysis
 
 correlation = data.corr()
@@ -70,3 +81,35 @@ features = ['Feature1', 'Feature2', 'Feature3']
 target = 'Unemployment Rate'
 
 
+#Split data into the traingin and testing set
+X = data[features]
+y = data[target]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+
+#Now we are training the model
+model = RandomForestRegressor(random_state = 42)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test) 
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse}")
+
+region_mean = data.groupby('Region')['Unemployment Rate'].mean().reset_index()
+print("\nRegion with the highest Unemployment rates: ")
+print(region_mean.head())
+
+
+# Now we will save the model
+plt.figure(figsize=(10, 6))
+region_mean.plot(kind = 'bar', color= 'Blue')
+plt.title('Average Unemplpoymant Rate by Region')
+plt.xlabel('Region')
+plt.ylabel('Average Unemployment Rate')
+plt.tight_layout()
+plt.savefig('unemployment_rate_by_region.png')
+plt.show()
